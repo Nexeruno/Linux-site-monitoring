fdfbec19e23230678c93b800e3194fc63b1d6220
root <root@DESKTOP-SNMGH55.localdomain>
2026-06-17 21:32:16 +0200

    Add local project test script

---
+++ test-project.sh
@@ -0,0 +1,81 @@
+#!/bin/env bash
+set -euo pipefail
+
+PROJECT_DIR="/root/training/site"
+
+check_bash_syntax(){
+    echo "Checking Bash syntax..."
+
+    bash -n "$PROJECT_DIR/site.sh"
+    bash -n "$PROJECT_DIR/run-sites.sh"
+    bash -n "$PROJECT_DIR/install.sh"
+    bash -n "$PROJECT_DIR/uninstall.sh"
+    bash -n "$PROJECT_DIR/health-check.sh"
+
+    echo "OK: Bash syntax check passed."
+}
+
+check_required_files(){
+    echo "Checking required files..."
+
+    local files=(
+        urls.txt
+        config.env
+        "urls.txt"
+        "config.env.example"
+        "run-sites.sh"
+        "site.sh"
+        "install.sh"
+        "uninstall.sh"
+        "health-check.sh"
+        "systemd/site-monitor.service"
+        "systemd/site-monitor.timer"
+    )
+
+    for file in "${files[@]}"; do
+     if [[ -f "$PROJECT_DIR/$file" ]]; then
+      echo "OK: $file exists"
+     else
+      echo "ERROR: $file is missing"
+      exit 1
+     fi
+     done
+}
+
+check_required_dirs(){
+    echo "Checking required directories..."
+
+    local dirs=(
+        logs
+        state
+        logrotate
+        systemd
+    )
+
+    for dir in "${dirs[@]}"; do
+     if [[ -d "$PROJECT_DIR/$dir" ]]; then
+     echo "OK: $dir exists"
+    else
+     echo "ERROR: $dir is missing"
+     exit 1
+    fi
+    done 
+}
+
+main(){
+    echo "Runing test project checks..."
+    echo
+
+    check_bash_syntax
+    echo
+
+    check_required_files
+    echo
+
+    check_required_dirs
+    echo
+
+    echo "All checks completed."
+}
+
+main "$@"
